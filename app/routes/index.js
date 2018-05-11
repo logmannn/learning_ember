cfcfc61688791b7b538073539f73047258366cc8
lt <logantanous@gmail.com>
2018-05-11 11:59:19 -0700

    stopping working on runtime bugs - not sure how to fix - everything seems to be working anyways

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,6 @@
-import Ember from 'ember';
-
-export default Ember.Route.extend({
+import Route from '@ember/routing/route';
+// not sure how to get this to work: https://github.com/ember-cli/ember-rfc176-data 
+export default Route.extend({
   model() {
     return Ember.RSVP.hash({
       rentals: this.store.findAll('rental'),
