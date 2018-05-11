481611e69b21be60fc2f2822fd66b5e7e405b643
lt <logantanous@gmail.com>
2018-05-11 13:49:45 -0700

    get helper method where reviews over 5 show up with an icon on rental page

--- app/helpers/rental-popularity.js
+++ app/helpers/rental-popularity.js
@@ -1,7 +1,12 @@
 import { helper } from '@ember/component/helper';
 
-export function rentalPopularity(params/*, hash*/) {
-  return params;
+export function rentalPopularity(params) {
+  var rental = params[0];
+
+  if(rental.get('reviews').get('length') >= 5) {
+    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-fire"></span>');
+  }
 }
+
 
 export default helper(rentalPopularity);
